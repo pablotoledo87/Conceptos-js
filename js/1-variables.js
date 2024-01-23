bc93ad5084e78c74da98ab60bdd77786e1ba9301
pablotoledo87 <toledo.pablo87@gmail.com>
2024-01-23 18:13:04 -0300

    agregamos un ejemplo de prompt con parSeInt

--- js/1-variables.js
+++ js/1-variables.js
@@ -11,7 +11,13 @@ document.write("<h1>Prueba de <br>titulo</h1>");
 document.write('Año: ' + anioActual + '<br>')
 document.write('Año: ' + anioActual)
 
-alert('Esto es una prueba de alerta')
+// alert('Esto es una prueba de alerta')
 
 let color = prompt('Ingrese un color: ')
 console.log(color)
+
+//solicitar al usiario un numero, y al numero indicado multiplicarlo por 15
+let numeroSeleccionado = prompt('ingrese un numero de 1 a 100')
+console.log(numeroSeleccionado)
+console.log(numeroSeleccionado *15)
+
