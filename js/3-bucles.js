aca7c0c2ef6da478b189b7bd71982b91fee75fbc
pablotoledo87 <toledo.pablo87@gmail.com>
2024-01-23 20:23:52 -0300

    agregamos un ejemplo de los 3 tipos de bucles

---
+++ js/3-bucles.js
@@ -0,0 +1,16 @@
+let renglon = 1;
+
+while(renglon <=10){
+document.write(`<p>Renglon numero ${renglon} </p>`);
+renglon = renglon + 1;
+}
+
+let cadena = 100;
+do{
+    document.write(`<p>Cadena numero ${cadena} </p>`);
+    cadena ++;
+}while(cadena <= 10)
+
+for(let contador =5 ; contador>=1; contador--){
+    document.write(`<p>Cuenta regresiva ${contador}</p>`)
+}
