c8390fb3b52c8f70c2bd65dc48abc3fdb78f8b77
pablotoledo87 <toledo.pablo87@gmail.com>
2024-01-23 17:59:22 -0300

    agregamos variables, mensajes por consola y pantalla

---
+++ js/1-variables.js
@@ -0,0 +1,17 @@
+let anioActual = 2023;
+
+console.log("este es un mensaje en consola")
+console.log(anioActual)
+console.log('Año ' + (anioActual + 2))
+
+anioActual = 2024;
+console.log(anioActual);
+
+document.write("<h1>Prueba de <br>titulo</h1>");
+document.write('Año: ' + anioActual + '<br>')
+document.write('Año: ' + anioActual)
+
+alert('Esto es una prueba de alerta')
+
+let color = prompt('Ingrese un color: ')
+console.log(color)
