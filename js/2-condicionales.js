ae6dfda92de5ecae820d82515e6db8ae298e6cf3
pablotoledo87 <toledo.pablo87@gmail.com>
2024-01-23 19:27:36 -0300

    agregamos un else if al ejercicio 6

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -6,12 +6,12 @@ let numero2 = parseInt( prompt('Ingrese un primer numero: '))
 // let numero3 = parseInt( prompt('Ingrese un primer numero: '))
 
 if(numero1>numero2){
-    console.log('el numero '+numero1 +' es mayor a numero '+numero2)
+    document.write('el numero '+numero1 +' es mayor a numero '+numero2)
 }else{
     if(numero2>numero1){
-        console.log('el numero '+numero2 +' es mayor a numero '+numero1)
+        document.write('el numero '+numero2 +' es mayor a numero '+numero1)
     }
     else{
-        console.log('Los numero ingresados son iguales ('+numero1+')')
+        document.write('Los numero ingresados son iguales ('+numero1+')')
     }
 }
