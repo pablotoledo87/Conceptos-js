7afca0560f28143f8781c2ef2c0cfa35ba3b1c12
pablotoledo87 <toledo.pablo87@gmail.com>
2024-01-23 22:18:50 -0300

    agregamos ejercicio 10 del tp 2

--- ejercicios/tp2-ejercicio10.js
+++ ejercicios/tp2-ejercicio10.js
@@ -1,13 +1,14 @@
 const filas = parseInt(prompt("Ingrese un numero de filas: "));
 const columnas = parseInt(prompt("Ingrese un numero de columnas: "));
 
-const totalCeldas = filas * columnas;
+let totalCeldas = filas * columnas;
 
 document.write(`<table><tbody>`);
 for(let indiceFilas = 0;indiceFilas < filas;indiceFilas ++ ){
     document.write(`<tr>`);
     for(let indiceColumnas = 0; indiceColumnas< columnas; indiceColumnas++){
-        document.write(`<td>35</td>`);
+        document.write(`<td>${totalCeldas}</td>`);
+        totalCeldas --;
     }
    
     document.write(`</tr>`);
